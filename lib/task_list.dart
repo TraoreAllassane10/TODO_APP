10ea35d54eb9de12c34bd4d150babe9c972e0861
Traore Allassane <traoreallassane2255@gmail.com>
2025-12-20 16:05:54 +0000

    Setup du projet

---
+++ lib/task_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TaskList extends StatefulWidget {
+  const TaskList({super.key});
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Liste des tâches"),),
+    );
+  }
+}
